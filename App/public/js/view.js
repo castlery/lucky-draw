e521df89d34ec41a28592b1c9bf8a813f82d97c8
Song Yangyu <flyfy1@gmail.com>
2016-07-28 17:48:10 +0800

    view with animation

--- App/public/js/view.js
+++ App/public/js/view.js
@@ -4,6 +4,9 @@
 (function($, window, document) {
 
   var machine = new Machine(function(poorMan) {
+    var rolling_times = 300;
+    var delay = 10;
+    var slow_down_since = rolling_times - 25;
 
     // TODO convert these to React style
     $('.main-container').removeClass('show animated fadeOutUp');
@@ -16,28 +19,26 @@
     });
 
     var stopping = false;
-    var delay = 60;
 
     var itemsTotal = $items.length; 
     var idMatched = -1;
 
-    try{
     // find the matched ID
     for(var i = 0 ; i < itemsTotal ; i++){
       if( $($items[i]).prop('id') == poorMan){ idMatched = i; break; }
     }
 
     // compute the starting counter value
-    // Row 300 times and then reach TODO: decide a proper 300
-    var start_value = idMatched - 300;
-    while(start_value < 0) start_value += itemsTotal;
-
-    console.log('start value: ', start_value, ', matched: ', idMatched, 'poor man: ', poorMan);
-    } catch(e){
-      console.error(e);
+    var start_value = idMatched - rolling_times, target_value = idMatched;
+    while(start_value < 0){
+      start_value += itemsTotal;
+      target_value += itemsTotal;
     }
 
+    // console.log('start value: ', start_value, ', matched: ', idMatched, 'poor man: ', poorMan);
+
     function loopAndLoop(counter) {
+      // console.log('loopAndLoop, counter: ', counter);
       // this is not animation...
       var $rolling = $('ul.rolling-list');
       var newItemsOrder = itemsArr.slice((counter - 2) % $items.length).concat(itemsArr.slice(0, (counter - 2) % $items.length));
@@ -61,24 +62,22 @@
         'height': winHeight/2.6
       });
 
-      if (stopping) {
-        if ($($items.get((counter) % $items.length)).prop('id') == poorMan) {
+      if (counter >= target_value) {
 
-          $('#winner-span').text(poorMan);
-          setTimeout(function() {
-
-            $('.main-container').removeClass('show animated fadeOutUp');
-            $('.main-container').addClass('hide');
-            $('#result-view-container').addClass('show animated fadeInDown');
-          }, 1000);
-          return;
-        } else if ($($items.get((counter+1) % $items.length)).prop('id') == poorMan) {
-          delay = 800;
-        } else if ($($items.get((counter+2) % $items.length)).prop('id') == poorMan) {
-          delay = 500;
-        } else if ($($items.get((counter+3) % $items.length)).prop('id') == poorMan) {
-          delay = 300;
-        }
+        // winning condition 
+        $('#winner-span').text(poorMan);
+        setTimeout(function() {
+          $('.main-container').removeClass('show animated fadeOutUp');
+          $('.main-container').addClass('hide');
+          $('#result-view-container').addClass('show animated fadeInDown');
+        }, 1000);
+        return;
+      } else if (counter == target_value - 1) {
+        delay = 800;
+      } else if (counter == target_value - 2) {
+        delay = 500;
+      } else if (counter == target_value - 3) {
+        delay = 300;
       }
 
       setTimeout(function() {
@@ -86,10 +85,7 @@
       }, delay);
 
       // Slow down incrementally
-      if(counter > 20) delay += 2;
-      if(counter > 40) {
-        stopping = true;
-      }
+      if(counter > slow_down_since) delay += 1;
     }
     loopAndLoop(start_value);
   });
@@ -209,6 +205,5 @@
       $(this).children('.text').removeClass('hide flipOutX');
       $(this).children('.text').addClass('show flipInX');
     });
-
   });
 })(jQuery, window, document);
