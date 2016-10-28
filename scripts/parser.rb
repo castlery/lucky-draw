aca981fdf2f0552f8a60d36828336b7f9e26de46
Song Yangyu <flyfy1@gmail.com>
2016-10-28 13:33:17 +0800

    style update for new lucky-draw

--- scripts/parser.rb
+++ scripts/parser.rb
@@ -1,10 +1,9 @@
 # Parse Ruby
 require 'csv'
 
-# host = 'http://stag.castlery.com:3006'
-host = 'http://localhost:3000'
-rows = CSV.read('./appointments.csv')
-
+host = 'http://localhost:3006'
+# host = 'http://localhost:3000'
+rows = CSV.read('/Users/songyy/Downloads/winnder_list_23.csv') 
 # Make HTTP request to load the data
 require 'http'
 
@@ -13,11 +12,15 @@ HTTP.post "#{host}/clearCandidates"
 HTTP.post "#{host}/setWithReplacement", 
   json: { isWithoutReplacement: 'true'}
 
+names = rows.map do |row| 
+  [row[1]] * row[2].to_i
+end.flatten.shuffle
 
 
 # load list of people into the lucky-draw machine
-rows.map do |row| 
+names.each do |name|
   HTTP.post "#{host}/addCandidate", 
-    json: { candidate: row[2] }
+    # json: { candidate: "#{row[0]}, #{row[1]}" }
+    json: { candidate: name }
 end
 # rand = HTTP.get 'http://localhost:3000/rand'
